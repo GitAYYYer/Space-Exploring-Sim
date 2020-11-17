ea5001d7ac7e2fc59334338049aa0f6d1354b5c5
Duc <duc.h@hotmail.com>
2020-11-17 19:10:28 +1100

    random resource on click

--- game.js
+++ game.js
@@ -3,13 +3,20 @@ function update() {
 }
 
 function listener (pointer, gameObject) {
-    console.log(`You clicked on ${JSON.stringify(gameObject,null,2)}`);
+    // console.log(`You clicked on ${JSON.stringify(gameObject,null,2)}`);
 
-        // Inventory += `${Counter}\n`;
-        //let resource = CurrentPlanet.resources[0];
-        //console.log(resource);
-        //Inventory[resource].Quantity += 1;
-        gameObject.angle += 10;
+    // Inventory += `${1}\n`;
+    let randomType = Math.floor(Math.random() * planetTypeData.length);
+    let randomResourceNumber = Math.floor(Math.random() * planetTypeData[randomType]['resources'].length);
+    let resourceName = planetTypeData[randomType]['resources'][randomResourceNumber];
+    console.log(randomType);
+    console.log(randomResourceNumber);
+    console.log(resourceName);
+    Inventory += `${resourceName}\n`;
+    //let resource = CurrentPlanet.resources[0];
+    //console.log(resource);
+    //Inventory[resource].Quantity += 1;
+    gameObject.angle += 10;
 
 }
 
